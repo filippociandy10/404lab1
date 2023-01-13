be50d3de847742b0390f12a49b64bf208e4aa9a1
filippociandy10 <filippo.ciandy10@gmail.com>
2023-01-13 11:55:24 -0700

    url req

--- lab1script.py
+++ lab1script.py
@@ -1,3 +1,5 @@
 import requests
-
-print(requests.get("https://www.google.com").text)
+url = "https://raw.githubusercontent.com/filippociandy10/404lab1/main/lab1script.py"
+r = requests.get(url)
+print(r.content)
+# print(requests.get("https://www.google.com").text)
