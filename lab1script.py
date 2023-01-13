d23163515cbf0b8343a29565f7aa0b89797d74fa
filippociandy10 <filippo.ciandy10@gmail.com>
2023-01-13 11:58:34 -0700

    changes to comment

--- lab1script.py
+++ lab1script.py
@@ -2,4 +2,3 @@ import requests
 url = "https://raw.githubusercontent.com/filippociandy10/404lab1/main/lab1script.py"
 r = requests.get(url)
 print(r.text)
-# print(requests.get("https://www.google.com").text)
